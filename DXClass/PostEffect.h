fbf9928f73baa529915148eb2c2fa1780bae3e7c
NoriomiKuboi <k020g1545@g.neec.ac.jp>
2022-05-23 15:24:58 +0900

    3-3まで実装(3-3に関しての問題は特になし)

--- DXClass/PostEffect.h
+++ DXClass/PostEffect.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Sprite.h"
+
 class PostEffect :
     public Sprite
 {
@@ -10,9 +11,20 @@ public:
     PostEffect();
 
     /// <summary>
+    /// 初期化
+    /// </summary>
+    void Init();
+
+    /// <summary>
     /// 描画コマンドの発行
     /// </summary>
     /// <param name="cmdList">コマンドリスト</param>
     void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+    // テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff;
+    // SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
